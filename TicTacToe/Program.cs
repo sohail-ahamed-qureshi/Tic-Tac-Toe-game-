588764d1d23f526d4fdebb204e7397d0e738a736
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-31 16:32:40 +0530

    [sohail] Add: initilize game board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,11 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to TicTacToe game");
+            //Console.WriteLine("Welcome to TicTacToe game");
+            TTTGame game = new TTTGame();
+            game.Welcome();
+            Console.WriteLine();
+            game.GameBoard();
         }
     }
 }
---
+++ TicTacToe/TTTGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TTTGame
+    {
+        //welcome message
+        public void Welcome()
+        {
+            Console.WriteLine("~~~~~~TIC TAC TOE~~~~~~");
+        }
+
+        public void GameBoard()
+        {
+            //game board intilized with empty spaces and
+            //zeroth index ignored
+            char[] board = new char[10];
+           
+            for (int i = 1; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+
+            ////displaying game board for trail
+            //foreach (char i in board)
+            //{
+            //    Console.WriteLine(i);
+            //}
+        }
+
+        public void Display()
+        {
+           
+        }
+        
+            
+
+
+
+    }
+}
